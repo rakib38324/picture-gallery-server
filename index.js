32ea8a6a8b597b66fc60142cbaf98476c55a3d09
rakib38324 <rakib38324@gmail.com>
2025-12-29 00:41:29 +0800

    update database

--- index.js
+++ index.js
@@ -10,10 +10,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// console.log("hello",process.env.DB_PASS)
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.qo03dll.mongodb.net/?appName=Cluster1`
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h8pz7ag.mongodb.net/?retryWrites=true&w=majority`
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
